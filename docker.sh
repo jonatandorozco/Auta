616c187090d0d3faa33d5bd5f22ec8c6b313f0e4
Jonatan Orozco <jonorozcoc@gmail.com>
2020-10-30 08:23:30 -0500

    Test docker script

---
+++ docker.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
