0e8570e93746c64c16c9d892d73a730c88ac2f97
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2021-01-11 10:42:37 -0500

    Changed starting console

--- doppler.sh
+++ doppler.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#/bin/sh
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/dopplerhq/doppler-deb stable main" | sudo tee /etc/apt/sources.list.d/dopplerhq-doppler.list
 sudo apt udpate
