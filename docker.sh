2ca113fd141c562f7a2c3eca10f991a96504bf42
Jonatan David Orozco Cantillo <jonorozcoc@gmail.com>
2021-01-11 10:39:48 -0500

    Removed upgrade command
    Generating conflicts and it is not needed

--- docker.sh
+++ docker.sh
@@ -1,7 +1,6 @@
 #/bin/sh
 
 sudo apt update
-sudo apt upgrade -y
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
